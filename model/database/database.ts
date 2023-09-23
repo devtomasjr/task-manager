a8624f47beee6804d9a9321cd0eb74b46e082522
devtomasjr <dev@tomasjr.com>
2023-09-22 21:34:12 -0300

    Add Database model

---
+++ model/database/database.ts
@@ -0,0 +1,36 @@
+import * as fs from 'fs';
+
+class Database {
+    private static readonly filePath = 'data/tasks.json';
+    private static readonly testFilePath = 'data/tasks.test.json';
+
+    public static read() {
+        try {
+            const data = JSON.parse(fs.readFileSync(this.filePath).toString());
+            data.response = { status: 200, message: 'Success' };
+            return data;
+        } catch (error) {
+            return { response: { status: 500, message: 'Unable to read database' } }
+        }
+    }
+
+    public static write(data: any) {
+        try {
+            fs.writeFileSync(this.filePath, JSON.stringify(data));
+            return { response: { status: 200, message: 'Success' } };
+        } catch (error) {
+            return { response: { status: 500, message: 'Unable to write database' } };
+        }
+    }
+
+    public static createFile(isTest: boolean = false) {
+        try {
+            fs.writeFileSync(isTest ? this.testFilePath : this.filePath, '{}');
+            return { response: { status: 200, message: 'Success' } };
+        } catch (error) {
+            return { response: { status: 500, message: 'Unable to create database file' } };
+        }
+    }
+}
+
+export default Database;
