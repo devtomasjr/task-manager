ca41099d891c36a6081c3ac6b048fccad169dad4
devtomasjr <dev@tomasjr.com>
2023-10-05 00:41:08 -0300

    Feat html tag to input

--- src/infra/public/counter.js
+++ src/infra/public/counter.js
@@ -2,7 +2,7 @@ var iTimedCount = 0;
 
 function timedCount() {
   onmessage = function(e) {
-    iTimedCount = e.data;
+    iTimedCount = e.data == 0 ? iTimedCount : e.data;
   }
   iTimedCount = iTimedCount + 1;
   postMessage(iTimedCount);
